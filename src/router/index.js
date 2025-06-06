399f486abed29d8a9209f998d95b45c09390e0ac
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-06-06 10:58:09 +0300

    feat: code splitting

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,19 @@
 import { path } from '@/configs/path'
-import Home from '@/views/home/Home.vue'
-import Notification from '@/views/notification/Notification.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
     path: path.home.path,
     name: path.home.name,
-    meta: {
-      layout: 'app-layout',
-    },
-    component: Home,
+    meta: { layout: 'app-layout' },
+    component: () => import(/* webpackChunkName: "home" */ '@/views/home/Home.vue'),
   },
   {
     path: path.notification.path,
     name: path.notification.name,
-    meta: {
-      layout: 'app-layout',
-    },
-    component: Notification,
+    meta: { layout: 'app-layout' },
+    component: () =>
+      import(/* webpackChunkName: "notification" */ '@/views/notification/Notification.vue'),
   },
 ]
 
